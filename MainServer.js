ced35a499eba9eb23aa73531792552b8f6b1c072
drox13 <dario.baron@uptc.edu.co>
2021-07-04 15:56:33 -0500

    se creo el modelo(lista) y se creo el servidor

---
+++ MainServer.js
@@ -0,0 +1,15 @@
+"use strict"
+const operacion = require("./modelList");
+const postMethod = require("./postMethod");
+
+const express = require('express');
+const app = express();
+const port = 5000;
+
+app.get('/', (req, res) => {
+    res.send('Hello World!')
+}); 
+
+app.listen(port, () => {
+    console.log(`Example app listening at http://localhost:${port}`);
+});
---
+++ modelList.js
@@ -0,0 +1,5 @@
+let miArray = ["Dario", "Alex", "Maria", "Mireya", "Juan"];
+
+module.exports = {
+    "miArray": miArray
+}
