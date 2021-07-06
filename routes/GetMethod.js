dcd4e3c5426a25dea4cddd34848e19d9c2ab2f31
jaimebp07 <72774804+jaimebp07@users.noreply.github.com>
2021-07-05 23:10:34 -0500

    se modifico metodo get

--- routes/GetMethod.js
+++ routes/GetMethod.js
@@ -1,10 +1,26 @@
 const model = require("../modelList");
 
 const express = require('express');
+const { request } = require("express");
 const router = express.Router()
 
-router.get('/prueba', (req,res)=>{
-    res.send(model.userList);
+router.get('/get/:username', (req, res) => {
+    console.log(req.params.username);
+    var exist = false;
+    var respuesta = "";
+    for (const nameObj in model.userList) {
+        console.log(`name: ${model.userList[nameObj]}`);
+        
+        if (req.params.username == model.userList[nameObj]) {
+            exist = true;
+            respuesta = respuesta +`${nameObj}, `;
+        }
+    }
+    if(exist){
+        res.send(`La persona ${req.params.username} esta existente en las posiciones ${respuesta}`);
+    } else{
+        res.send(`La persona ${req.params.username} no esta exitente el la lista`);
+    }
 });
 
 module.exports = router;
